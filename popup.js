4678151fa2db9879898a963612fda7f20bafa530
xoxaaaalopo <aoto15530@gmail.com>
2025-06-22 10:30:36 -0700

    Made popup interactive

---
+++ popup.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.querySelector(".fade-btn");
+    button.addEventListener("click", () => {
+        chrome.tabs.query({active: true, currentWindow: true}, ([tab]) => {
+            chrome.scripting.executeScript({
+                target: {tabId: tab.id},
+                func: () => {
+                    const video = document.querySelector("video");
+                    if (!video) return;
+                    startFade();
+                }
+            });
+        });
+    });
+});
+
+function startFade(video) {
+    setTimeout(() => {
+        const initialVolume = video.volume;
+        let step = 0;
+        const fade = setInterval(() => {
+            step++;
+            video.volume = Math.max(0, initialVolume * (60 - step)/ 60);
+            if (step >= 60) clearInterval(fade);
+        }, 1000);
+    }, 1000);
+}
